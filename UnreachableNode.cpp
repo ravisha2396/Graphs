08f8d3798f90a90cabe6605aed0444d8407f0a90
ravi shankar <ravisha2396@gmail.com>
2021-05-04 16:10:01 +0530

    Added description comments to UnreachableNodes program.

--- UnreachableNode.cpp
+++ UnreachableNode.cpp
@@ -1,6 +1,22 @@
 /*
 	Program to find the number of unreachable nodes in a graph.
 	
+	An undirected graph is a graph in which the direction of an edge between the nodes doesn't matter.
+	
+	Example of Undirected Graph:
+	
+	(Node 1) -- (Node 2)
+	
+	Example of Directed Graph:
+	
+	(Node 1) --> (Node 2)
+	
+	Example of Unreachable node in an undirected graph:
+	
+	(Node 1) -- (Node 2) -- (Node 3)    (Node 4) -- (Node 5)    (Node 6)
+
+	In the graph above, we have a graph of 6 nodes, with Node 1 as root node. The root node is connected to Node 2 and Node 3, however, there is no edge linking Node 1, Node 2 and Node 3 with Node 4, Node 5 or Node 6. Hence, Node 4, Node 5 and Node 6 are unreachable from root node i.e Node 1.
+	
 	Author: Ravi Shankar
 	Date: 04-05-2021
 	
@@ -9,7 +25,6 @@
 #include<iostream>
 #include<stack>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
